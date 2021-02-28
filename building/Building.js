738b32d55767a15848648694d379ae6bafb9d937
800147 <800147@gmail.com>
2021-02-28 20:04:21 +0300

    stupid Safari

--- building/Building.js
+++ building/Building.js
@@ -10,6 +10,11 @@ class Draggable {
     this._onDragEnd = null;
     this._buildingObj = null;
 
+    this._dragStart = this._dragStart.bind(this);
+    this._drag = this._drag.bind(this);
+    this._dragEnd = this._dragEnd.bind(this);
+    this._getPos = this._getPos.bind(this);
+
     if (domElement) {
       this.init(domElement, onDragEnd, buildingObj);
     }
@@ -28,14 +33,14 @@ class Draggable {
     }
   }
 
-  _dragStart = (e) => {
+  _dragStart(e) {
     if (this._pointerStart !== null) {
       return;
     }
 
     e.stopPropagation();
 
-    if (this._buildingObj.status !== Building.STATUSES.ACTIVE) {
+    if (this._buildingObj.status !== BUILDING_STATUSES.ACTIVE) {
       return;
     }
 
@@ -63,7 +68,7 @@ class Draggable {
     }
   }
 
-  _drag = (e) => {
+  _drag(e) {
     if (!e.touches && this._touchEventMode === true) {
       return;
     }
@@ -81,7 +86,7 @@ class Draggable {
     this._domElem.style.setProperty('--y', y - this._pointerStart.y);
   }
 
-  _dragEnd = (e) => {
+  _dragEnd(e) {
     if (!e.touches && this._touchEventMode === true) {
       return;
     }
@@ -137,101 +142,103 @@ class Draggable {
   }
 };
 
+
+const BUILDING_STATUSES = {
+  LOADING: 'loading',
+  ACTIVE: 'active',
+  DISABLED: 'disabled'
+};
+
+const BUILDING_PHASES = [
+  {// 0
+    startTime: 0,
+    endTime: 0,
+    endPhase: 0,
+    pauseStatus: BUILDING_STATUSES.ACTIVE,
+    yesItem: 0,
+    noPhase: 1,
+    yesPhase: 2,
+    image: 0
+  },
+  {// 1 - no
+    startTime: 0,
+    endTime: 1,
+    endPhase: 0,
+    status: BUILDING_STATUSES.DISABLED
+  },
+  {// 2 - cement
+    startTime: 1.2,
+    endTime: 7.8,
+    endPhase: 2,
+    status: BUILDING_STATUSES.DISABLED,
+    pauseStatus: BUILDING_STATUSES.ACTIVE,
+    yesItem: 1,
+    noPhase: 3,
+    yesPhase: 4,
+    image: 1
+  },
+  {// 3 - no
+    startTime: 8,
+    endTime: 9,
+    endPhase: 2,
+    status: BUILDING_STATUSES.DISABLED
+  },
+  {// 4 - walls
+    startTime: 9.4,
+    endTime: 14.4,
+    endPhase: 4,
+    status: BUILDING_STATUSES.DISABLED,
+    pauseStatus: BUILDING_STATUSES.ACTIVE,
+    yesItem: 2,
+    noPhase: 5,
+    yesPhase: 6,
+    image: 2
+  },
+  {// 5 - no
+    startTime: 14.4,
+    endTime: 15.4,
+    endPhase: 4,
+    status: BUILDING_STATUSES.DISABLED
+  },
+  {// 6 - roof
+    startTime: 15.6,
+    endTime: 18,
+    endPhase: 6,
+    status: BUILDING_STATUSES.DISABLED,
+    pauseStatus: BUILDING_STATUSES.ACTIVE,
+    yesItem: 3,
+    noPhase: 7,
+    yesPhase: 8,
+    image: 3
+  },
+  {// 7 - no
+    startTime: 18,
+    endTime: 19,
+    endPhase: 6,
+    status: BUILDING_STATUSES.DISABLED
+  },
+  {// 8 - light
+    startTime: 19.2,
+    endTime: 23.2,
+    endPhase: 8,
+    status: BUILDING_STATUSES.DISABLED,
+    pauseStatus: BUILDING_STATUSES.ACTIVE,
+    yesItem: 4,
+    yesPhase: 9,
+    image: 4
+  },
+  {// 9 - windows
+    startTime: 23.4,
+    endTime: 40.2,
+    endPhase: 9,
+    status: BUILDING_STATUSES.DISABLED,
+    pauseStatus: BUILDING_STATUSES.DISABLED,
+    image: 5
+  }
+];
+
+
 class Building {
-  static STATUSES = {
-    LOADING: 'loading',
-    ACTIVE: 'active',
-    DISABLED: 'disabled'
-  };
-
-  static PHASES = [
-    {// 0
-      startTime: 0,
-      endTime: 0,
-      endPhase: 0,
-      pauseStatus: Building.STATUSES.ACTIVE,
-      yesItem: 0,
-      noPhase: 1,
-      yesPhase: 2,
-      image: 0
-    },
-    {// 1 - no
-      startTime: 0,
-      endTime: 1,
-      endPhase: 0,
-      status: Building.STATUSES.DISABLED
-    },
-    {// 2 - cement
-      startTime: 1.2,
-      endTime: 7.8,
-      endPhase: 2,
-      status: Building.STATUSES.DISABLED,
-      pauseStatus: Building.STATUSES.ACTIVE,
-      yesItem: 1,
-      noPhase: 3,
-      yesPhase: 4,
-      image: 1
-    },
-    {// 3 - no
-      startTime: 8,
-      endTime: 9,
-      endPhase: 2,
-      status: Building.STATUSES.DISABLED
-    },
-    {// 4 - walls
-      startTime: 9.4,
-      endTime: 14.4,
-      endPhase: 4,
-      status: Building.STATUSES.DISABLED,
-      pauseStatus: Building.STATUSES.ACTIVE,
-      yesItem: 2,
-      noPhase: 5,
-      yesPhase: 6,
-      image: 2
-    },
-    {// 5 - no
-      startTime: 14.4,
-      endTime: 15.4,
-      endPhase: 4,
-      status: Building.STATUSES.DISABLED
-    },
-    {// 6 - roof
-      startTime: 15.6,
-      endTime: 18,
-      endPhase: 6,
-      status: Building.STATUSES.DISABLED,
-      pauseStatus: Building.STATUSES.ACTIVE,
-      yesItem: 3,
-      noPhase: 7,
-      yesPhase: 8,
-      image: 3
-    },
-    {// 7 - no
-      startTime: 18,
-      endTime: 19,
-      endPhase: 6,
-      status: Building.STATUSES.DISABLED
-    },
-    {// 8 - light
-      startTime: 19.2,
-      endTime: 23.2,
-      endPhase: 8,
-      status: Building.STATUSES.DISABLED,
-      pauseStatus: Building.STATUSES.ACTIVE,
-      yesItem: 4,
-      yesPhase: 9,
-      image: 4
-    },
-    {// 9 - windows
-      startTime: 23.4,
-      endTime: 40.2,
-      endPhase: 9,
-      status: Building.STATUSES.DISABLED,
-      pauseStatus: Building.STATUSES.DISABLED,
-      image: 5
-    }
-  ];
-
   constructor({
     domElem,
     video,
@@ -248,11 +255,16 @@ class Building {
     this._rightPage = rightPage;
     this._enterFrameInterval = null;
 
+    this._onCurrentPageChange = this._onCurrentPageChange.bind(this);
+    this._start = this._start.bind(this);
+    this._onEnterFrame = this._onEnterFrame.bind(this);
+    this._onSetItem = this._onSetItem.bind(this);
+
     const items = this._domElem.querySelectorAll('.building__item');
     const platform = this._domElem.querySelectorAll('.building__platform')[0];
 
     this._phase = 0;
-    this._status = Building.STATUSES.LOADING;
+    this._status = BUILDING_STATUSES.LOADING;
 
     items.forEach((item, index) => {
       const onDragEnd = (x, y) => {
@@ -280,23 +292,23 @@ class Building {
     this._resetAll();
   }
 
-  _onCurrentPageChange = ({ detail: { currentPage }}) => {
+  _onCurrentPageChange({ detail: { currentPage }}) {
     if (currentPage !== this._pageIndex - 1) {
       this._resetAll();
     }
   };
 
-  _start = () => {
+  _start() {
     if (this._enterFrameInterval) {
       return;
     }
 
     this._enterFrameInterval = setInterval(this._onEnterFrame);
-    this._setStatus(Building.PHASES[this._phase].pauseStatus);
+    this._setStatus(BUILDING_PHASES[this._phase].pauseStatus);
   };
 
-  _onEnterFrame = () => {
-    const { endTime, endPhase } = Building.PHASES[this._phase];
+  _onEnterFrame() {
+    const { endTime, endPhase } = BUILDING_PHASES[this._phase];
 
     if (this._video.paused) {
       return;
@@ -308,7 +320,7 @@ class Building {
 
     this._setPhase(endPhase);
 
-    const { endTime: endPhaseEndTime, pauseStatus: endPhasePauseStatus, pauseStatus, image } = Building.PHASES[this._phase];
+    const { endTime: endPhaseEndTime, pauseStatus: endPhasePauseStatus, pauseStatus, image } = BUILDING_PHASES[this._phase];
 
     this._setStatus(pauseStatus);
     this._video.pause();
@@ -320,8 +332,8 @@ class Building {
     }
   };
 
-  _onSetItem = (item) => {
-    const { yesItem, yesPhase, noPhase } = Building.PHASES[this._phase];
+  _onSetItem(item) {
+    const { yesItem, yesPhase, noPhase } = BUILDING_PHASES[this._phase];
 
     if (item === yesItem) {
       this._setPhase(yesPhase);
@@ -329,7 +341,7 @@ class Building {
       this._setPhase(noPhase);
     }
 
-    const { startTime, status } = Building.PHASES[this._phase];
+    const { startTime, status } = BUILDING_PHASES[this._phase];
 
     this._setStatus(status);
     this._video.play();
@@ -365,7 +377,7 @@ class Building {
     this._video.currentTime = 0;
     this._setPhase(0);
     this._setImages(0);
-    this._setStatus(Building.STATUSES.ACTIVE);
+    this._setStatus(BUILDING_STATUSES.ACTIVE);
   }
 
   get status() {
